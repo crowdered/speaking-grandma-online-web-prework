d29957f559d721e0563829839886c40a07a7c8a4
Mark Crowder <crowdered@gmail.com>
2019-03-05 17:46:50 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,5 +4,6 @@ def speak_to_grandma(phrase)
   elsif phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == "WHAT DID YOU EAT TODAY?"
-    puts "NO, NOT SINCE 1938!
+    puts "NO, NOT SINCE 1938!"
+  end
 
