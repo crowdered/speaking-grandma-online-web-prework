d3d26fe12b5aabd0aea086110ef1a9f5bfb3b11b
Mark Crowder <crowdered@gmail.com>
2019-03-05 17:44:52 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,5 +2,6 @@ def speak_to_grandma(phrase)
   if !phrase.upcase
     puts "HUH?! SPEAK UP, SONNY!"
   elsif phrase == "I LOVE YOU GRANDMA!"
-    puts ""
+    puts "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase == 
 
