f5c5a9917c64d827a979464c3cd4badc510669b0
Mark Crowder <crowdered@gmail.com>
2019-03-05 17:37:34 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,2 @@
 def speak_to_grandma
 
-if speak_to_grandma == "I LOVE YOU GRANDMA"
-  puts "I LOVE YOU TOO, PUMPKIN!"
-else speak_to_grandma == "Hi Nana, how are you?"
-  puts "HUH?! SPEAK UP, SONNY!"
-elseif speak_to_grandma == "Hi Nana, how are you?".upcase
-  puts 
-  
